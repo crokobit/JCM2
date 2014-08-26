2385076ccdf712c462fd07562bbee536577b3adf
crokobit <crokobit@gmail.com>
2014-08-26 12:23:35 +0800

    init

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module JCM2
+  class Application < Rails::Application
+    config.generators do |generate|
+      generate.helper false
+      generate.decorator false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.controller_specs false
+      generate.test_framework  :test_unit, fixture: false
+    end
+  end
+end
